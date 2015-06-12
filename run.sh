6c13fb4e4c9d29f7d00de0190b1122274b27c9ed
Yi-Hsiu Liao <nee0224256667@yahoo.com.tw>
2015-06-12 14:28:17 +0800

    remove space from line start

--- run.sh
+++ run.sh
@@ -93,7 +93,7 @@ command="fstcompose $tmpdir/input.fst Lexicon.fst | \
 command+=" | sed "
 command+=" -e 's/<s>//g' -e 's/<\/s>//g' -e 's/SIL//g' "
 command+=$(while read phone; do echo " -e \"s/\b${phone}\b/ /g\""; done < phone_list)
-command+=" | tr -s ' ' | sort | uniq"
+command+=" | tr -s ' ' | sed -e 's/^ //g' | sort | uniq"
 
 eval $command
 # compose Lexicon.fst and input.fst and output the shortest path
